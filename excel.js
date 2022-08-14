fb19c80a624e09fa3af2d6cb5601d650a64aff8d
Maor Kadosh <maork@venn.city>
2022-08-15 02:07:05 +0300

    introducing... questionsInSheet!!!! 😲

--- excel.js
+++ excel.js
@@ -1,27 +1,47 @@
-import { read as readXLSX, utils } from "./deps/xlsx.mjs";
+import { read as readXLSX } from "./deps/xlsx.mjs";
 
-function rowsInSheet(sheet, columnCount, rowCount) {
-  for (let letter = 0; letter < columnCount; letter++) {
-    const letterString = (letter + 10).toString(36).toUpperCase();
-    for (let number = 1; number <= rowCount; number++)
-      console.log(`${letterString}${number}`);
+/**
+ * Turn Sheet into an array of topics with questions.
+ * For example, turns the following table:
+ *
+ *   Topic1 topic2 topic3
+ *   a      b      c
+ *   d      e      f
+ *   g      h      i
+ *
+ * Into:
+ *
+ *   [
+ *     { title: 'Topic1', questions: { '1': 'a', '2': 'd', '3': 'g' } },
+ *     { title: 'topic2', questions: { '1': 'b', '2': 'e', '3': 'h' } },
+ *     { title: 'topic3', questions: { '1': 'c', '2': 'f', '3': 'i' } }
+ *   ]
+ */
+function questionsInSheet(sheet) {
+  const topics = {};
+
+  // skip first element because it's the special value `!ref`
+  for (const k of Object.keys(sheet).slice(1, -1)) {
+    const cellContent = sheet[k].v;
+
+    // columns are letters, used to identify topics
+    // rows are numbers, represent difficulty
+    const [, columnNum, rowNum] = k.match(/([A-Z]+?)([0-9]+)/);
+
+    // first row contains topic titles
+    if (rowNum === "1") {
+      topics[columnNum] = { title: cellContent, questions: {} };
+    } else {
+      const difficulty = parseInt(rowNum) - 1;
+      topics[columnNum].questions[difficulty] = cellContent;
+    }
   }
-}
 
-/** Example `!ref` property: `A1:C4` */
-function refToCounts(ref) {
-  const matches = ref.match(/\w+:(\w+)(\d+)/);
-  console.log(ref);
-  console.log(matches);
-  return {
-    // parsing an a-z character into its ascii code
-    rowCount: parseInt(matches[2]),
-    columnCount: parseInt(matches[1], 36) - 9,
-  };
+  return Object.values(topics);
 }
 
 export function parseXlsx(uploadedXlsxFile) {
   const xlsx = readXLSX(uploadedXlsxFile);
   const firstSheet = Object.values(xlsx.Sheets)[0];
-  return refToCounts(firstSheet["!ref"])
+  return questionsInSheet(firstSheet);
 }
