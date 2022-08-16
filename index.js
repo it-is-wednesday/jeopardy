0e74a3748cf534afd32207d90cab0ec9554c8a44
Maor Kadosh <maork@venn.city>
2022-08-17 00:54:42 +0300

    hide file input after submitting

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import { IndexPage, QuestionPage } from "./ui.js";
 import { parseXlsx } from "./excel.js";
 
 const fileInput = document.getElementById("xlsx");
+
 fileInput.addEventListener("change", async () => {
+  fileInput.hidden = true;
   const uploadedFile = await fileInput.files[0].arrayBuffer();
   const topics = parseXlsx(uploadedFile);
+
   route(document.querySelector("main"), "/index", {
     "/index": IndexPage(topics),
     "/q/:topicId/:difficulty": QuestionPage(topics),
